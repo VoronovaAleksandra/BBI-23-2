8dce5fa25602ebec2b4af93ff4689bc205fecf51
VoronovaAleksandra <159521678+VoronovaAleksandra@users.noreply.github.com>
2024-05-24 12:07:19 +0300

    Кр1

---
+++ 1CW_2t_var3.cs
@@ -0,0 +1,159 @@
+﻿//2 
+public abstract class Shape
+{
+    public abstract double Volume();
+
+    public abstract void Print();
+}
+
+public class Cube : Shape
+{
+    public double A { get; private set; }
+
+    public Cube(double a)
+    {
+        A = a;
+    }
+
+    public override double Volume()
+    {
+        return Math.Pow(A, 3);
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"Cube: A: {A}, Volume: {Volume()}");
+    }
+}
+
+public class Sphere : Shape
+{
+    public double R { get; private set; }
+
+    public Sphere(double r)
+    {
+        R = r;
+    }
+
+    public override double Volume()
+    {
+        return 4 * Math.PI * Math.Pow(R, 3) / 3;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"Sphere: R: {R}, Volume: {Volume()}");
+    }
+}
+
+public class Cylinder : Shape
+{
+    public double R { get; private set; }
+    public double H { get; private set; }
+
+    public Cylinder(double r, double h)
+    {
+        R = r;
+        H = h;
+    }
+
+    public override double Volume()
+    {
+        return H * Math.PI * Math.Pow(R, 2);
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"Cylinder: R: {R}, H: {H}, Volume: {Volume()}");
+    }
+}
+
+public class Program
+{
+    private static void SortedShapesByVolume(Shape[] shapes)
+    {
+        for (int i = 0; i < shapes.Length; i++)
+        {
+            for (int j = i + 1; j < shapes.Length; j++)
+            {
+                if (shapes[i].Volume() < shapes[j].Volume())
+                {
+                    Shape shape = shapes[i];
+                    shapes[i] = shapes[j];
+                    shapes[j] = shape;
+                }
+            }
+        }
+    }
+
+    private static void PrintShapes(Shape[] shapes)
+    {
+        foreach (Shape shape in shapes)
+        {
+            shape.Print();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Cube[] cubes = new[]
+        {
+            new Cube(1),
+            new Cube(2),
+            new Cube(3),
+            new Cube(4),
+            new Cube(5)
+        };
+        Sphere[] spheres = new[]
+        {
+            new Sphere(1),
+            new Sphere(2),
+            new Sphere(3),
+            new Sphere(4),
+            new Sphere(5)
+        };
+        Cylinder[] cylinders = new[]
+        {
+            new Cylinder(2, 10),
+            new Cylinder(4, 8),
+            new Cylinder(6, 6),
+            new Cylinder(8, 4),
+            new Cylinder(10, 2)
+        };
+
+        SortedShapesByVolume(cubes);
+        SortedShapesByVolume(spheres);
+        SortedShapesByVolume(cylinders);
+
+        Console.WriteLine("Cubes:");
+        PrintShapes(cubes);
+        Console.WriteLine("Spheres:");
+        PrintShapes(spheres);
+        Console.WriteLine("Cylinders:");
+        PrintShapes(cylinders);
+
+        Shape[] shapes = new Shape[15];
+        int k = 0;
+        foreach (Cube cube in cubes)
+        {
+            shapes[k++] = cube;
+        }
+
+        foreach (Sphere sphere in spheres)
+        {
+            shapes[k++] = sphere;
+        }
+
+        foreach (Cylinder cylinder in cylinders)
+        {
+            shapes[k++] = cylinder;
+        }
+
+        SortedShapesByVolume(shapes);
+        Console.WriteLine("Shapes: ");
+        PrintShapes(shapes);
+    }
+}
+
+
+
---
+++ CW1_1t_var3.cs
@@ -0,0 +1,74 @@
+﻿//1 создать структуру Vector ,представляющую собой вектор, составленный из двух точек Вар 3
+public struct Dot
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public int Z { get; private set; }
+
+    public Dot(int[] mas)
+    {
+        X = mas[0];
+        Y = mas[1];
+        Z = mas[2];
+    }
+
+    public override string ToString()
+    {
+        return $"X: {X}, Y: {Y}, Z: {Z}";
+    }
+}
+
+public struct Vector
+{
+    public Dot A { get; private set; }
+    public Dot B { get; private set; }
+
+    public Vector(int[,] matrix)
+    {
+        A = new Dot(new int[] { matrix[0, 0], matrix[0, 1], matrix[0, 2] });
+        B = new Dot(new int[] { matrix[1, 0], matrix[1, 1], matrix[1, 2] });
+    }
+
+    public double Distance()
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2) + Math.Pow(B.Z - A.Z, 2));
+    }
+
+    public override string ToString()
+    {
+        return $"A: {A}, B: {B}, Distance: {Distance()}";
+    }
+}
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Vector[] vectors = new[]
+        {
+            new Vector(new int[,]{{10, 2, 3}, {2, 3, 4}}),
+            new Vector(new int[,]{{3, 4, 5}, {2, 3, 4}}),
+            new Vector(new int[,]{{6, -10, 3}, {1, 5, 4}}),
+            new Vector(new int[,]{{1, 0, 3}, {2, 3, 0}}),
+            new Vector(new int[,]{{8, 2, 1}, {7, 7, 7}})
+        };
+        for (int i = 0; i < vectors.Length; ++i)
+        {
+            for (int j = i + 1; j < vectors.Length; j++)
+            {
+                if (vectors[i].Distance() < vectors[j].Distance())
+                {
+                    Vector vector = vectors[i];
+                    vectors[i] = vectors[j];
+                    vectors[j] = vector;
+                }
+            }
+        }
+
+        foreach (var vector in vectors)
+        {
+            Console.WriteLine(vector);
+        }
+    }
+}
